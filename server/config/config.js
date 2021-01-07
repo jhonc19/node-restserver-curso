d4d101ebe5c92f486bdbf735f79bf3052542f6eb
Jhon Choque <jhonc.choquec@gmail.com>
2021-01-06 21:25:22 -0500

    Add variable config Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:gGFAAqNj4QmOLneW@cluster0.98n6c.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
